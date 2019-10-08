3fc1e3cc418f2fe9429a5825a66f3301014d2ec5
Danielle B <danibaxx@hotmail.com>
2019-10-07 18:46:54 -0600

    day 2

--- src/App.js
+++ src/App.js
@@ -1,73 +1,55 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios';
+// imported with {} due to no default export in file
+import { useInput } from './utilities/input';
 
 import './App.css';
 
-export default class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      // set the default value to husky
-      breed: 'husky',
-      images: []
-    }
-  }
+function App(props) {
+  const [breed, setBreed] = useInput('doberman');
+  const [images, setImages] = useState([]);
 
-  componentDidMount() {
-    this.fetchDogImages()
-  }
+  useEffect(() => {
+    setImages([])
+    fetchDogImages()
+  }, [breed])
 
-// params that is given to Update to help with infinite looping 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.breed !== this.state.breed) {
-      this.setState({
-        images: []
-      })
-      this.fetchDogImages()
-    }
-  }
-
-  fetchDogImages = () => {
+  const fetchDogImages = () => {
     axios
-    .get(`https://dog.ceo/api/breed/${this.state.breed}/images`)
+    .get(`https://dog.ceo/api/breed/${breed}/images`)
     .then(response => {
       // console.log("response", response.data.message)
-      this.setState({
-        images: response.data.message
+      setImages(response.data.message)
       })
-    })
     .catch(error => {
       console.log('Error', error)
     })
   }
 
-  handleChange = (event) => {
-    this.setState({
-      breed: event.target.value
-    });
-  };
-
-  render() {
-    return (
-      <>
-      <h1>The Dog App</h1>
-
-        <select value={this.state.breed} onChange={this.handleChange}>
-          <option value='doberman'>Doberman</option>
-          <option value='husky'>Husky</option>
-          <option value='corgi'>Corgi</option>
-        </select>
-        <div>
-          {this.state.images.map((image, index) => (
-            <img 
-              key={index} 
-              src={image} 
-              alt='Dog' 
-            />
-          ))}
-        </div>
-      </>
-    )
+  const handleChange = (event) => {
+    setBreed(event.target.value)
   }
-}
 
+  return (
+    <>
+    <h1>The Dog App</h1>
+
+      <select value={breed} onChange={handleChange}>
+        <option value='doberman'>Doberman</option>
+        <option value='husky'>Husky</option>
+        <option value='corgi'>Corgi</option>
+      </select>
+      <div>
+        {images.map((image, index) => (
+          <img 
+            key={index} 
+            src={image} 
+            alt='Dog' 
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default App;
