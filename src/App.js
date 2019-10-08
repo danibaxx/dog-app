1d24787c8ba206105920c4c7d8abdcb54ded28b5
Danielle B <danibaxx@hotmail.com>
2019-10-07 18:03:42 -0600

    day 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      // set the default value to husky
+      breed: 'husky',
+      images: []
+    }
+  }
+
+  componentDidMount() {
+    this.fetchDogImages()
+  }
+
+// params that is given to Update to help with infinite looping 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.breed !== this.state.breed) {
+      this.setState({
+        images: []
+      })
+      this.fetchDogImages()
+    }
+  }
+
+  fetchDogImages = () => {
+    axios
+    .get(`https://dog.ceo/api/breed/${this.state.breed}/images`)
+    .then(response => {
+      // console.log("response", response.data.message)
+      this.setState({
+        images: response.data.message
+      })
+    })
+    .catch(error => {
+      console.log('Error', error)
+    })
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      breed: event.target.value
+    });
+  };
+
+  render() {
+    return (
+      <>
+      <h1>The Dog App</h1>
+
+        <select value={this.state.breed} onChange={this.handleChange}>
+          <option value='doberman'>Doberman</option>
+          <option value='husky'>Husky</option>
+          <option value='corgi'>Corgi</option>
+        </select>
+        <div>
+          {this.state.images.map((image, index) => (
+            <img 
+              key={index} 
+              src={image} 
+              alt='Dog' 
+            />
+          ))}
+        </div>
+      </>
+    )
+  }
 }
 
-export default App;
